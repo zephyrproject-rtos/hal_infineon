8bf7dc41dbfa870d840d30b9b55db844bc81987f
Nazar Palamar <nazar.palamar@infineon.com>
2023-02-15 10:10:45 +0200

    hal: infineon: Add WHD + blobs support for WIFI FW/CLM for CYW43xx device.
    - Added WiFi Host Driver (WHD) ver.2.5.0.15307
      WHD patches to prevent macros redefined warnings:
      -- replaced BIT_MASK to WHD_BIT_MASK
      -- replaced ARRAY_SIZE to WHD_ARRAY_SIZE
    
    - Added blob support for WIFI FW/CLM for CYW43xx device:
     -- CYW43012
     -- CYW4343W
     -- CYW43438
     -- CYW43439
     -- CYW4373
    
    Signed-off-by: Nazar Palamar <nazar.palamar@infineon.com>

---
+++ wifi-host-driver/WiFi_Host_Driver/resources/firmware/COMPONENT_43439/43439a0-mfgtest_bin.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019, Cypress Semiconductor Corporation, All Rights Reserved
+ * SPDX-License-Identifier: LicenseRef-PBL
+ *
+ * This file and the related binary are licensed under the
+ * Permissive Binary License, Version 1.0 (the "License");
+ * you may not use these files except in compliance with the License.
+ *
+ * You may obtain a copy of the License here:
+ * LICENSE-permissive-binary-license-1.0.txt and at
+ * https://www.mbed.com/licenses/PBL-1.0
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "wiced_resource.h"
+
+#ifdef WLAN_MFG_FIRMWARE
+#if defined(CY_STORAGE_WIFI_DATA)
+CY_SECTION_WHD(CY_STORAGE_WIFI_DATA) __attribute__((used))
+#endif
+const unsigned char wifi_mfg_firmware_image_data[223628] = {
+        #include <cyw43xx_fw_blob.inc>
+};
+const resource_hnd_t wifi_mfg_firmware_image = { RESOURCE_IN_MEMORY, 223628, {.mem = { (const char *) wifi_mfg_firmware_image_data }}};
+#endif /* WLAN_MFG_FIRMWARE */
